c30917a2d56aaee5181ef387c96905fd5addc388
julian1 <julian.atkinson71@gmail.com>
2016-03-19 12:43:56 +1100

    update

--- examples/server.c
+++ examples/server.c
@@ -11,13 +11,17 @@
 
 void error(char *msg)
 {
+  // this is horrid
     perror(msg);
     exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
+     int sockfd, newsockfd, portno;//, clilen;
+
+     socklen_t  clilen;
+
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
@@ -28,34 +32,36 @@ int main(int argc, char *argv[])
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
-     // bzero((char *) &serv_addr, sizeof(serv_addr));
 
     int enable = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
         error("setsockopt(SO_REUSEADDR) failed");
 
 
+     // bzero((char *) &serv_addr, sizeof(serv_addr));
      memset(&serv_addr, 0, sizeof(serv_addr));
 
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
      if (newsockfd < 0) 
           error("ERROR on accept");
-     bzero(buffer,256);
+
+     //bzero(buffer,256);
+    memset(&buffer, 0, 256); 
      n = read(newsockfd,buffer,255);
      if (n < 0) error("ERROR reading from socket");
+
      printf("Here is the message: %s\n",buffer);
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
+
      return 0; 
 }
