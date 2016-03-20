43786b5ec1532e43d91627219d2f97ebf180c890
julian1 <julian.atkinson71@gmail.com>
2016-03-20 19:50:29 +1100

    organize into directories

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 rm logger.o reactor.o examples/*.out
 
-gcc -Wall -c logger.c -I./ -o logger.o || exit
-gcc -Wall -c reactor.c -I./ -o reactor.o || exit   # change name core?
-gcc -Wall -c ureactor.c -I./ -o ureactor.o || exit
-gcc -Wall -c signal.c -I./ -o signal.o || exit
+for i in src/*.c; do
+  gcc -Wall $i -c -I./include -o "${i%.c}.o" || exit
+done
+#exit
+
 
 #for i in examples/*.c; do
 for i in examples/read.c examples/signal.c; do
-  gcc -Wall $i logger.o reactor.o ureactor.o signal.o -I./ -o "${i%.c}.out" || exit
+  gcc -Wall $i src/*.o  -I./include -o "${i%.c}.out" || exit
 done
 
