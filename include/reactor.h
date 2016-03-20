b2b8b09f0a87db0305cb9ebc3a70c60451bdefc2
julian1 <julian.atkinson71@gmail.com>
2016-03-20 22:11:17 +1100

    tidy

--- include/reactor.h
+++ include/reactor.h
@@ -4,24 +4,29 @@
 #include <demux.h>
 #include <signal_.h>
 
-
 typedef struct Reactor Reactor;
 
 Reactor *reactor_create(); 
+
 void reactor_destroy(Reactor *);
 
 int reactor_run_once(Reactor *d);
+
 void reactor_run(Reactor *d);
+
 void reactor_cancel_all(Reactor *d);
 
-// typedef struct Handler Handler;
 //void reactor_cancel_handler(Handler *h);
 
 void reactor_on_timer(Reactor *d, int timeout, void *context, Demux_callback callback);
+
 void reactor_on_read_ready( Reactor *d, int fd, int timeout, void *context, Demux_callback callback);
+
 void reactor_on_write_ready(Reactor *d, int fd, int timeout, void *context, Demux_callback callback);
 
 void reactor_register_signal(Reactor *, int signal);
+
 void reactor_deregister_signal(Reactor *, int signal);
+
 void reactor_on_signal(Reactor *, int timeout, void *context, Signal_callback callback);
 
